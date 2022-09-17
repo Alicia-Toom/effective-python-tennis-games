d6631ca1520369b755aeefadccd2abe0fbdace91
Alicia Toom <alicia.chumchai@gmail.com>
2022-09-17 09:52:09 +0200

    Add comments for tennis_game2

--- tennis_game2.py
+++ tennis_game2.py
@@ -11,17 +11,18 @@ class TennisGame2:
         self.player2Name = player2Name
         self.p1points = 0
         self.p2points = 0
-        
+
     def won_point(self, playerName):
         """Add one point to the score of the playerName argument"""
         if playerName == self.player1Name:
             self.P1Score()
         else:
             self.P2Score()
-    
+
     def score(self):
         """Count the current score and return the result"""
         result = ""
+        # Handle case when both players have the same score before reaching fourty
         if (self.p1points == self.p2points and self.p1points < 3):
             if (self.p1points==0):
                 result = "Love"
@@ -30,12 +31,12 @@ class TennisGame2:
             if (self.p1points==2):
                 result = "Thirty"
             result += "-All"
-        
+        # Handle case when both players have the same score at fourty
         if (self.p1points==self.p2points and self.p1points>2):
             result = "Deuce"
-        
         P1res = ""
         P2res = ""
+        # Handle case when player 1 start getting the score than 0, but the players 2 has 0
         if (self.p1points > 0 and self.p2points==0):
             if (self.p1points==1):
                 P1res = "Fifteen"
@@ -43,9 +44,10 @@ class TennisGame2:
                 P1res = "Thirty"
             if (self.p1points==3):
                 P1res = "Forty"
-            
+
             P2res = "Love"
             result = P1res + "-" + P2res
+        # Handle case when player 2 start getting the score than 0, but the players 1 has 0
         if (self.p2points > 0 and self.p1points==0):
             if (self.p2points==1):
                 P2res = "Fifteen"
@@ -53,11 +55,10 @@ class TennisGame2:
                 P2res = "Thirty"
             if (self.p2points==3):
                 P2res = "Forty"
-            
+
             P1res = "Love"
             result = P1res + "-" + P2res
-        
-        
+        # Handle case when player 1 has higher score than player 2
         if (self.p1points>self.p2points and self.p1points < 4):
             if (self.p1points==2):
                 P1res="Thirty"
@@ -68,6 +69,7 @@ class TennisGame2:
             if (self.p2points==2):
                 P2res="Thirty"
             result = P1res + "-" + P2res
+        # Handle case when player 2 has higher than player 1
         if (self.p2points>self.p1points and self.p2points < 4):
             if (self.p2points==2):
                 P2res="Thirty"
@@ -78,34 +80,34 @@ class TennisGame2:
             if (self.p1points==2):
                 P1res="Thirty"
             result = P1res + "-" + P2res
-        
+        # Handle case when duece and player 1 has higher score than player 2
         if (self.p1points > self.p2points and self.p2points >= 3):
             result = "Advantage " + self.player1Name
-        
+         # Handle case when duece and player 2 has higher score than player 1
         if (self.p2points > self.p1points and self.p1points >= 3):
             result = "Advantage " + self.player2Name
-        
+         # Handle case when player 1 has more than 2 points than player 2
         if (self.p1points>=4 and self.p2points>=0 and (self.p1points-self.p2points)>=2):
             result = "Win for " + self.player1Name
+         # Handle case when player 2 has more than 2 points than player 1
         if (self.p2points>=4 and self.p1points>=0 and (self.p2points-self.p1points)>=2):
             result = "Win for " + self.player2Name
         return result
-    
+
     def SetP1Score(self, number):
-        """counting the score for set 1"""
+        """Calculate the score for player 1"""
         for i in range(number):
             self.P1Score()
-    
+
     def SetP2Score(self, number):
-        """couting the score for set2"""
+        """Calculate the score for player 2"""
         for i in range(number):
             self.P2Score()
-    
+
     def P1Score(self):
-        """couting the set score for player1"""
+        """Increase the score by 1 point for player 1"""
         self.p1points +=1
-    
-    
+
     def P2Score(self):
-        """couting the set score for player2"""
+        """Increase the score by 1 point for player 2"""
         self.p2points +=1
