54aa205807894731aad6491eda16778e8e027948
Alicia Toom <alicia.chumchai@gmail.com>
2022-09-17 14:29:29 +0200

    Add comments to both class and functions using Numpy style

--- tennis_refactor.py
+++ tennis_refactor.py
@@ -1,34 +1,77 @@
-""" TODO """
+""" Refactor code from the old class TennisGame1 and TennisGame2
+ into two new classes; Player, and TennisGame
+"""
 
 from typing import Dict
 
 # pylint: disable=too-few-public-methods
-class Player():
-    """ TODO """
+class Player:
+    """Define class that represent a tennis player
 
-    def __init__(self, name):
+    Parameters
+    ----------
+    name : str
+        the name parameter used for the name of the player
+    Returns
+    -------
+    None
+    """
+    def __init__(self, name: str) -> None:
         self.name = name
 
-class TennisGame():
-    """ TODO """
+class TennisGame:
+    """The tennis game itself where the score and the winner will be calculated
 
-    def __init__(self, player1: Player, player2: Player):
+    Parameters
+    ----------
+    player1 : str
+        The first player name
+    player2 : str
+        The second player name
+
+    Returns
+    -------
+    None
+    """
+    def __init__(self, player1: Player, player2: Player) -> None:
         self.player1 = player1
         self.player2 = player2
         self.score_card: Dict[Player, int] = {player1: 0, player2: 0}
 
-    def add_point_to_player(self, player: Player):
-        """Add one point to the score of the player argument"""
+    def add_point_to_player(self, player: Player) -> None:
+        """Add one point to the score of the player argument
+
+        Parameters
+        ----------
+        player : str
+            The players name
+
+        Returns
+        -------
+        None
+        """
         self.score_card[player] = self.score_card[player] + 1
 
-    def get_current_score(self):
-        """Count the current score and return the result"""
+    def get_current_score(self) -> str:
+        """Count the current score and return the result
+
+        Parameters
+        ----------
+        player : str
+            The players name
+
+        Returns
+        -------
+        str
+            the current score in natural language
+        """
         result = ""
         player1_score = self.score_card[self.player1]
         player2_score = self.score_card[self.player2]
         score_terms: Dict[int, str] = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Fourty"}
-
+        # Handle case when both players have the same score
         if player1_score == player2_score:
+            # Return deuce when both players have score at 40 or above 40
             if player1_score >= 4:
                 result = "Deuce"
             else:
@@ -42,6 +85,7 @@ class TennisGame():
                 result = f"Win for {self.player1.name}"
             elif player2_score >= 4 and (player2_score - player1_score >= 2):
                 result = f"Win for {self.player2.name}"
+            # Handle case when non of players have won and continue playing
             else:
                 result = f"{score_terms[player1_score]} - {score_terms[player2_score]}"
 
